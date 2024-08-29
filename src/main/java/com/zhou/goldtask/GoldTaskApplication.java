28fe9c0f6ab93b24f560276495cff8a35627c1dd
Zhouwanxing <never_zhouwx@yeah.net>
2024-08-29 14:16:16 +0800

    1

---
+++ src/main/java/com/zhou/goldtask/GoldTaskApplication.java
@@ -0,0 +1,13 @@
+package com.zhou.goldtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhou/goldtask/GoldTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhou.goldtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
