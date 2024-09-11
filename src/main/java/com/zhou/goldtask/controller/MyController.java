0b62f855d3c9e5e40e1ed25ef74a1e05aa8e9489
Zhouwanxing <never_zhouwx@yeah.net>
2024-09-11 13:52:29 +0800

    1

--- src/main/java/com/zhou/goldtask/controller/MyController.java
+++ src/main/java/com/zhou/goldtask/controller/MyController.java
@@ -5,15 +5,15 @@ import com.zhou.goldtask.entity.MyConfig;
 import com.zhou.goldtask.service.MyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @Controller
 @Slf4j
 @RestController
+@RequestMapping("/page")
+@CrossOrigin
 public class MyController {
     @Resource
     private MyService myService;
@@ -22,5 +22,12 @@ public class MyController {
     public String initConfig(@RequestBody JSONObject config) {
         myService.initConfig(config.toBean(MyConfig.class));
         return "success";
+    }
+
+    @GetMapping("/testA")
+    public JSONObject testA() {
+        JSONObject a = new JSONObject();
+        a.putOpt("a", "ccc");
+        return a;
     }
 }
