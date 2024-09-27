2094f940f716fa24ed9b7230d48bc1557bc83cc8
Zhouwanxing <never_zhouwx@yeah.net>
2024-09-27 18:12:24 +0800

    优化UrlService中的URL处理逻辑在UrlService`类中

--- src/main/java/com/zhou/goldtask/service/UrlService.java
+++ src/main/java/com/zhou/goldtask/service/UrlService.java
@@ -68,7 +68,6 @@ public class UrlService {
                 return new String(Base64.getDecoder().decode(en));
             }
         } catch (Exception ignored) {
-
         }
         return null;
     }
