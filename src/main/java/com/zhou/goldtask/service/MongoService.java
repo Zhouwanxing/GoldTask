0ae0e9210c30800ebc487004bd1529b5ebff916c
Zhouwanxing <never_zhouwx@yeah.net>
2024-09-27 17:14:34 +0800

    增加增删改查

--- src/main/java/com/zhou/goldtask/service/MongoService.java
+++ src/main/java/com/zhou/goldtask/service/MongoService.java
@@ -1,5 +1,6 @@
 package com.zhou.goldtask.service;
 
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
@@ -15,6 +16,9 @@ import org.bson.codecs.pojo.PojoCodecProvider;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
@@ -57,5 +61,86 @@ public class MongoService {
             client.close();
         }
         return true;
+    }
+
+    public <T> T findOneById(String _id, Class<T> tClass) {
+        return findOneByCon(idToJson(_id), tClass);
+    }
+
+    public <T> T findOneByCon(JSONObject condition, Class<T> tClass) {
+        MongoClient client = getMongoClient();
+        if (client == null) {
+            return null;
+        }
+        try {
+            return client.getDatabase(dbName).getCollection(tClass.getAnnotation(TableName.class).value(), tClass).find(Document.parse(condition.toString())).first();
+        } catch (Exception e) {
+            log.warn("", e);
+            return null;
+        }
+    }
+
+    public <T> List<T> findListByCon(JSONObject condition, Class<T> tClass) {
+        MongoClient client = getMongoClient();
+        if (client == null) {
+            return new ArrayList<>();
+        }
+        try {
+            return client.getDatabase(dbName).getCollection(tClass.getAnnotation(TableName.class).value(), tClass).find(Document.parse(condition.toString())).into(new ArrayList<>());
+        } catch (Exception e) {
+            log.warn("", e);
+            return new ArrayList<>();
+        }
+    }
+
+    private JSONObject idToJson(Object _id) {
+        return new JSONObject().putOpt("_id", _id);
+    }
+
+    public <T> void updateOneById(String _id, JSONObject setData, Class<T> tClass) {
+        updateOneByCon(idToJson(_id), setData, tClass);
+    }
+
+    public <T> void updateOneByCon(JSONObject con, JSONObject setData, Class<T> tClass) {
+        MongoClient client = getMongoClient();
+        if (client == null) {
+            return;
+        }
+        try {
+            client.getDatabase(dbName).getCollection(tClass.getAnnotation(TableName.class).value(), tClass)
+                    .updateOne(Document.parse(con.toString()), Document.parse(setData.toString()));
+        } catch (Exception e) {
+            log.warn("", e);
+        }
+    }
+
+    public <T> void updateAllByCon(JSONObject con, JSONObject setData, Class<T> tClass) {
+        MongoClient client = getMongoClient();
+        if (client == null) {
+            return;
+        }
+        try {
+            client.getDatabase(dbName).getCollection(tClass.getAnnotation(TableName.class).value(), tClass)
+                    .updateMany(Document.parse(con.toString()), Document.parse(setData.toString()));
+        } catch (Exception e) {
+            log.warn("", e);
+        }
+    }
+
+    public <T> void deleteOneById(String _id, Class<T> tClass) {
+        deleteOneByCon(idToJson(_id), tClass);
+    }
+
+    private <T> void deleteOneByCon(JSONObject idToJson, Class<T> tClass) {
+        MongoClient client = getMongoClient();
+        if (client == null) {
+            return;
+        }
+        try {
+            client.getDatabase(dbName).getCollection(tClass.getAnnotation(TableName.class).value(), tClass)
+                    .deleteMany(Document.parse(idToJson.toString()));
+        } catch (Exception e) {
+            log.warn("", e);
+        }
     }
 }
