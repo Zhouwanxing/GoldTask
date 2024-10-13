cad0d3310c4c77e87ba2f0da1976a129442387d8
Zhouwanxing <never_zhouwx@yeah.net>
2024-10-13 11:16:53 +0800

    提醒调整

--- src/main/java/com/zhou/goldtask/service/OnlineService.java
+++ src/main/java/com/zhou/goldtask/service/OnlineService.java
@@ -26,11 +26,11 @@ public class OnlineService {
         }
         if (DateUtil.thisHour(true) < 9) {
             if (info.getRecord().size() == 0) {
-                taskService.remindTask("打卡", "");
+                taskService.remindTask("打卡", "上班打卡");
             }
         } else {
             if (info.getRecord().size() != 2) {
-                taskService.remindTask("打卡", "");
+                taskService.remindTask("打卡", "下班打卡");
             }
         }
     }
