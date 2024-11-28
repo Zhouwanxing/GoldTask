17a55e59bea4dfb2d41feee044e6a692f690ea67
Zhouwanxing <never_zhouwx@yeah.net>
2024-11-28 15:06:08 +0800

    feat(UserService): 添加 mongo 数据库连接测试功能
    - 在 UserService 类中添加了 mongoTest 方法
    - 使用 Lombok 的 log4j 注解添加日志记录
    -记录数据库中用户数量，以验证数据库连接是否正常

--- src/main/java/com/zhou/goldtask/service/UserService.java
+++ src/main/java/com/zhou/goldtask/service/UserService.java
@@ -2,17 +2,20 @@ package com.zhou.goldtask.service;
 
 import com.zhou.goldtask.entity.UserEntity;
 import com.zhou.goldtask.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 @Service
+@Slf4j
 public class UserService {
     @Resource
     private UserRepository userRepository;
 
     public void mongoTest() {
-        userRepository.count();
+        long count = userRepository.count();
+        log.info("mongo 检测:" + count);
     }
 
 
