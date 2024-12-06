be6053e894667c66050835e917c7b2a538c25800
Zhouwanxing <never_zhouwx@yeah.net>
2024-12-06 13:45:12 +0800

    refactor(GoldEntity): 重构黄金实体类
    - 移除了未使用的 TableName 注解
    -将 Document 注解的参数从 "my_gold" 改为 "my_gold"
    - 添加了 AllArgsConstructor、NoArgsConstructor、Setter 和 Getter 注解- 初始化了 zdf、zss 和 ccb 字段为 0

--- src/main/java/com/zhou/goldtask/entity/GoldEntity.java
+++ src/main/java/com/zhou/goldtask/entity/GoldEntity.java
@@ -1,21 +1,21 @@
 package com.zhou.goldtask.entity;
 
-import com.zhou.goldtask.annotation.TableName;
-import lombok.Builder;
-import lombok.Data;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
 @ToString
-@TableName("my_gold")
 @Document("my_gold")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class GoldEntity {
     @Id
     private String _id;
-    private int zdf;
-    private int zss;
-    private int ccb;
+    private int zdf = 0;
+    private int zss = 0;
+    private int ccb = 0;
 }
