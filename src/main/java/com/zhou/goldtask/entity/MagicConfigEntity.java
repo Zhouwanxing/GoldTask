f8b847f0d6de384da257902e89586a10f4b104a5
Zhouwanxing <never_zhouwx@yeah.net>
2025-11-04 17:58:22 +0800

    2

---
+++ src/main/java/com/zhou/goldtask/entity/MagicConfigEntity.java
@@ -0,0 +1,17 @@
+package com.zhou.goldtask.entity;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@ToString
+@Document("my_magic_config")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class MagicConfigEntity {
+    private String file_path;
+    private String file_content;
+}
